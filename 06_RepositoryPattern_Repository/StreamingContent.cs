ee78a0f7f0d7f255db91a1e290d1e44d88a3d20e
michael Davidson <meridianproservice@outlook.com>
2021-04-27 19:27:28 -0400

    Add project files.

---
+++ 06_RepositoryPattern_Repository/StreamingContent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_RepositoryPattern_Repository
+{
+    public enum GenreType
+    {
+        Horror = 1,
+        RomCom,
+        SciFi,
+        Documentry,
+        Drama,
+        Action
+    }
+    //Plain Old C# Object -- POCO
+    public class StreamingContent
+    {
+        public string Title { get; set; }       //prop tab tab will give a set up for creating a field
+        public string Description { get; set; }
+        public string MaturityRating { get; set; }
+        public double StarRating { get; set; }
+        public bool IsFamilyFriendly {get; set;} 
+        public GenreType TypeOfGenre { get; set; }
+
+        public StreamingContent() { } // two contruct one will be and over load, one will be empty 
+
+        public StreamingContent(string title, string description, string maturityRating, double starRating, bool isFamilyFriendly, GenreType genre)   // ctor tab tab new contructor base on the named content ex;"Streaming Content"
+        {
+            Title = title;
+            Description = description;
+            MaturityRating = maturityRating;
+            StarRating = starRating;
+            IsFamilyFriendly = isFamilyFriendly;
+            TypeOfGenre = genre;
+
+        }
+    }
+}
---
+++ 06_Repository_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("06_Repository_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06_Repository_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("266ff987-84e2-49b0-9a05-3d5af99b0492")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
